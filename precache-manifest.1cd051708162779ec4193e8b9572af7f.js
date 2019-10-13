f80c579d9691c4a65868b0624ec6e8c2d01a8ee6
Damon Bolesta <dbolesta@gmail.com>
2019-10-12 21:23:24 -0400

    Updates

---
+++ precache-manifest.1cd051708162779ec4193e8b9572af7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58847d59292153abeaca9efbff1fd123",
+    "url": "/damon-portfolio/index.html"
+  },
+  {
+    "revision": "aaaa8d54af3ed49b5cf0",
+    "url": "/damon-portfolio/static/css/main.35f6fc64.chunk.css"
+  },
+  {
+    "revision": "f10166a9c5661da6b568",
+    "url": "/damon-portfolio/static/js/2.ac0bec56.chunk.js"
+  },
+  {
+    "revision": "aaaa8d54af3ed49b5cf0",
+    "url": "/damon-portfolio/static/js/main.7a48f536.chunk.js"
+  },
+  {
+    "revision": "667594421a885e5193b7",
+    "url": "/damon-portfolio/static/js/runtime~main.c2d52ace.js"
+  },
+  {
+    "revision": "3cff9a2f481c6642a4af5f5860398d5a",
+    "url": "/damon-portfolio/static/media/star.3cff9a2f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dbolesta/damon-portfolio/precache-manifest.725c198ae1e177922a4bb8fb3595f78f.js"
+  "/damon-portfolio/precache-manifest.1cd051708162779ec4193e8b9572af7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dbolesta/damon-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/damon-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
